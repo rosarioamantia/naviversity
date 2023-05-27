f84066b1d4570fa37751212232a2044a3beac28c
rosar <rosario.amantia.dev@gmail.com>
2023-05-27 16:03:59 +0200

    Initial commit

---
+++ app/src/main/java/com/rosario/naviversity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rosario.naviversity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
