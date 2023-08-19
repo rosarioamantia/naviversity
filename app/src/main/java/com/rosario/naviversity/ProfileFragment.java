9d3141a5d6864c752e2d3f60accefb623e8fd94d
rosar <rosario.amantia@outlook.it>
2023-08-19 21:03:02 +0200

    management of car view data and inserting

--- app/src/main/java/com/rosario/naviversity/ProfileFragment.java
+++ app/src/main/java/com/rosario/naviversity/ProfileFragment.java
@@ -8,15 +8,23 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +32,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,11 +49,16 @@ public class ProfileFragment extends Fragment {
     User currentUser;
     FirebaseDatabase mDatabase;
     DatabaseReference dbReference;
-    TextView userName, userSurname, userEmail;
+    TextView userNameTxt, userSurnameTxt, userEmailTxt;
     RatingBar ratingBar;
     ConstraintLayout carConstraintLayout;
     String[] carColors, carModels;
     AutoCompleteTextView carColorTxt, carModelTxt;
+    Button btnModify, btnLogout, btnConfirm;
+    EditText carPlateTxt;
+    ArrayAdapter<String> carModelsAdapter;
+    ArrayAdapter<String> carColorsAdapter;
+    TextInputLayout carModelLayout, carPlateLayout, carColorLayout;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -95,44 +111,55 @@ public class ProfileFragment extends Fragment {
         fUser = mAuth.getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance();
         dbReference = mDatabase.getReference();
-        userName = view.findViewById(R.id.user_name);
-        userSurname = view.findViewById(R.id.user_surname);
-        userEmail = view.findViewById(R.id.user_mail);
+        userNameTxt = view.findViewById(R.id.user_name);
+        userSurnameTxt = view.findViewById(R.id.user_surname);
+        userEmailTxt = view.findViewById(R.id.user_mail);
         ratingBar = view.findViewById(R.id.rating_bar);
-        carConstraintLayout = view.findViewById(R.id.car_constratint_layout);
+        carConstraintLayout = view.findViewById(R.id.car_constraint_layout);
         carColors = getResources().getStringArray(R.array.car_colors);
         carModels = getResources().getStringArray(R.array.car_models);
-
+        btnModify = view.findViewById(R.id.btn_modifiy);
+        btnConfirm = view.findViewById(R.id.btn_confirm);
+        carModelTxt = view.findViewById(R.id.car_model_txt);
+        carColorTxt = view.findViewById(R.id.car_color_txt);
+        carPlateTxt = view.findViewById(R.id.car_plate_txt);
+        btnLogout = view.findViewById(R.id.btn_logout);
+        carModelLayout = view.findViewById(R.id.car_model_input_layout);
+        carColorLayout = view.findViewById(R.id.car_color_input_layout);
+        carPlateLayout = view.findViewById(R.id.car_plate_input_layout);
+        carColorsAdapter = new ArrayAdapter<>(getContext(),
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                carColors);
+        carModelsAdapter = new ArrayAdapter<>(getContext(),
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                carModels);
         setUIData(view);
-
         return view;
     }
-
     public void setUIData(View view){
         dbReference.child("user").child(fUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 currentUser = snapshot.getValue(User.class);
-                currentUser.setId(snapshot.getKey());
-                userName.setText(currentUser.getName());
-                userSurname.setText(currentUser.getSurname());
-                userEmail.setText(fUser.getEmail());
-                ratingBar.setRating(4.0f);
-
+                currentUser.setId(fUser.getUid());
+                userNameTxt.setText(currentUser.getName());
+                userSurnameTxt.setText(currentUser.getSurname());
+                userEmailTxt.setText(fUser.getEmail());
+                float score = currentUser.getScore();
+                int ratingReceived = currentUser.getRatingReceived();
+                float scoreToShow = (float) Math.ceil(score / ratingReceived);
+                ratingBar.setRating(scoreToShow);
 
                 if(currentUser.isCarOwner()){
+                    carSwitch.setChecked(true);
                     carConstraintLayout.setVisibility(View.VISIBLE);
-                    ArrayAdapter<String> carColorsAdapter = new ArrayAdapter<>(getContext(),
-                            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                            carColors);
-                    ArrayAdapter<String> carModelsAdapter = new ArrayAdapter<>(getContext(),
-                            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                            carModels);
-                    carModelTxt = view.findViewById(R.id.car_model_txt);
-                    carColorTxt = view.findViewById(R.id.car_color_txt);
                     carColorTxt.setText(currentUser.getCar().getColor());
-                }else{
-                    carConstraintLayout.setVisibility(View.GONE);
+                    carModelTxt.setText(currentUser.getCar().getModel());
+                    carPlateTxt.setText(currentUser.getCar().getPlate());
+
+                    carConstraintLayout.setVisibility(View.VISIBLE);
+                    carModelTxt.setAdapter(carModelsAdapter);
+                    carColorTxt.setAdapter(carColorsAdapter);
                 }
             }
 
@@ -141,5 +168,134 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.getInstance().signOut();
+                Toast.makeText(getContext(), "Utente logout ", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        carSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    carConstraintLayout.setVisibility(View.VISIBLE);
+                    carModelTxt.setAdapter(carModelsAdapter);
+                    carColorTxt.setAdapter(carColorsAdapter);
+
+                }else{
+                    //aggiungi controllo su corse create dall'utente
+                    carConstraintLayout.setVisibility(View.GONE);
+                    if(currentUser.getCar() != null){
+                        deleteUserCar();
+                        carModelTxt.setText(null);
+                        carColorTxt.setText(null);
+                        carPlateTxt.setText(null);
+                    }
+                }
+            }
+        });
+
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                carModelLayout.setEnabled(true);
+                carColorLayout.setEnabled(true);
+                carPlateLayout.setEnabled(true);
+                btnConfirm.setVisibility(View.VISIBLE);
+                btnModify.setVisibility(View.GONE);
+                carColorTxt.setText(null);
+                carModelTxt.setText(null);
+                carPlateTxt.setText(null);
+
+                btnConfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(checkIsFilledCarValues()){
+                            if(currentUser.isCarOwner()){
+                                if(checkAreChangedCarValues()){
+                                    updateUserCar();
+                                }
+                            }else{
+                                addUserCar();
+                            }
+                            btnConfirm.setVisibility(View.GONE);
+                            btnModify.setVisibility(View.VISIBLE);
+                            carModelLayout.setEnabled(false);
+                            carColorLayout.setEnabled(false);
+                            carPlateLayout.setEnabled(false);
+                        }
+                    }
+                });
+            }
+        });
+    }
+    public void deleteUserCar(){
+        currentUser.setCar(null);
+        currentUser.setCarOwner(false);
+        Map<String, Object> userValues = currentUser.toMap();
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        childUpdates.put("/user/" + currentUser.getId() , userValues);
+        dbReference.updateChildren(childUpdates);
+        Toast.makeText(getContext(), "Automobile eliminata correttamente", Toast.LENGTH_SHORT).show();
+    }
+    public void updateUserCar(){
+        String carPlate = carPlateTxt.getText().toString();
+        String carColor = carColorTxt.getText().toString();
+        String carModel = carModelTxt.getText().toString();
+        Car newCar = new Car(carModel, carPlate, carColor);
+        currentUser.setCar(newCar);
+        Map<String, Object> userValues = currentUser.toMap();
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        childUpdates.put("/user/" + currentUser.getId() , userValues);
+        dbReference.updateChildren(childUpdates);
+        Toast.makeText(getContext(), "Automobile aggiornata correttamente", Toast.LENGTH_SHORT).show();
+    }
+    public void addUserCar(){
+        String carPlate = carPlateTxt.getText().toString();
+        String carColor = carColorTxt.getText().toString();
+        String carModel = carModelTxt.getText().toString();
+        Car newCar = new Car(carModel, carPlate, carColor);
+        currentUser.setCar(newCar);
+        currentUser.setCarOwner(true);
+        Map<String, Object> userValues = currentUser.toMap();
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        childUpdates.put("/user/" + currentUser.getId() , userValues);
+        dbReference.updateChildren(childUpdates);
+        Toast.makeText(getContext(), "Automobile aggiunta correttamente", Toast.LENGTH_SHORT).show();
+    }
+    public boolean checkIsFilledCarValues(){
+        String carModel = carModelTxt.getText().toString();
+        String carColor = carColorTxt.getText().toString();
+        String carPlate = carPlateTxt.getText().toString();
+        if(carModel.isEmpty()){
+            Toast.makeText(getContext(), "Devi inserire un modello di auto", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(carColor.isEmpty()){
+            Toast.makeText(getContext(), "Devi inserire un colore di auto", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(carPlate.isEmpty()){
+            Toast.makeText(getContext(), "Devi inserire una targa di auto", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+    public boolean checkAreChangedCarValues(){
+        String carPlate = carPlateTxt.getText().toString();
+        String carColor = carColorTxt.getText().toString();
+        String carModel = carModelTxt.getText().toString();
+        Car currentCar = currentUser.getCar();
+        if(!currentCar.getPlate().equals(carPlate) || !currentCar.getColor().equals(carColor) || !currentCar.getModel().equals(carModel)){
+            return true;
+        }
+        return false;
     }
 }
